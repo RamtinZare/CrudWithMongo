4f300aad05e3f97cd165794710252f56ceee52b0
ramtin <ramtin.zare@gmail.com>
2024-04-27 10:24:28 +0330

    add entity

---
+++ src/main/java/com/example/demo/entity/CustomerEntity.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document("test01")
+public class CustomerEntity {
+    @Id
+    private String customerId;
+    private String customerName;
+    private String customerEmail;
+    private String customerMeli;
+    private String customerPhone;
+}
